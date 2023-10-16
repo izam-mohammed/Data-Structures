64ca7499728f1c40c7289278ef0cf6c76a8e6465
Izam Mohammed <izamdeveloper1@gmail.com>
2023-10-16 12:02:50 +0530

    did some Recursion as well as String

---
+++ Recursion/problems/factorial.py
@@ -0,0 +1,9 @@
+
+def factorial(n):
+    if n == 0:
+        return 1
+    
+    return n * factorial(n-1)
+
+for i in range(10):
+    print(f'factorial {i} = {factorial(i)}')
---
+++ String/problems/valid_anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def valid_anagram(self, s1:str, s2:str) -> bool:
+        if len(s1) != len(s2):
+            return False
+        
+        s1_d = {i:sum([1 for k in s1 if k==i]) for i in set(list(s1))}
+        s2_d = {i:sum([1 for k in s2 if k==i]) for i in set(list(s2))}
+        
+        return s1_d == s2_d
+
+obj = Solution()
+print(obj.valid_anagram('hai hai', 'hai iah'))
